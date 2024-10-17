ded8c31c6a80911d8349cc11d2a0a00a5fa3b34f
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-10-17 16:14:44 -0600

    Add files via upload

---
+++ dicvacio.py
@@ -0,0 +1,21 @@
+print(" ")
+print("alexa guadalupe ramirez manzo 1205 3-W")
+print(" ")
+persona = {}# diccionario 
+#informacion a preguntar
+nombre = input("¿Cuál es tu nombre? ") 
+persona['nombre'] = nombre# se añade a diccionario
+print(persona)
+#informacion a preguntar
+edad = input("¿Cuál es tu edad? ")
+persona['edad'] = edad# se añade a diccionario
+print(persona)
+#informacion a preguntar
+sexo = input("¿Cuál es tu sexo? (M/F) ")
+persona['sexo'] = sexo# se añade a diccionario
+print(persona)
+#informacion a preguntar
+telefono = input("¿Cuál es tu número de teléfono? ")
+persona['telefono'] = telefono# se añade a diccionario
+print(persona)
+print(" ")
